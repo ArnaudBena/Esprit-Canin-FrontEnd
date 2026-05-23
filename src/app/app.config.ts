092b017fc6e980e259959793507bb4f1e7c05f23
Arnaud BENACQUISTA <arnaud.benacquista@gmail.com>
2026-05-23 15:41:04 +0200

    chore: enregistrement Locale FR + LOCALE_ID par défaut, cela causé un bug sur les dates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor]))
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
   ]
 };
